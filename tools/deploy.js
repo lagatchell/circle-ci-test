c1d0c8e5865f1045c2e6464f6fc7fe00b5ec6f4a
lagatchell <lagatchell@live.com>
2019-10-20 18:41:15 -0400

    ci: add ftp connect

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,4 +1,20 @@
 const FTPClient = require('ftp');
-const env = process.env;
+const ENV = process.env;
+const USERNAME = ENV.FTP_USERNAME;
+const PASSWORD = ENV.FTP_PASSWORD;
+const HOST = ENV.FTP_HOST;
+const PORT = ENV.FTP_SERVER_PORT || 21;
 
-console.log(env.TEST_VAR);
+const client = new FTPClient();
+client.on('ready', function () {
+  console.log("CONNECTED");
+  client.end();
+});
+
+
+client.connect({
+  host: HOST,
+  port: PORT,
+  user: USERNAME,
+  password: PASSWORD,
+});
